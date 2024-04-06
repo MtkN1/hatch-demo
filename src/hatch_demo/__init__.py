c7a2ad9132d779bfda1f090279cc0f84c79ca4dd
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2024-04-06 06:52:15 +0000

    Add tests

--- src/hatch_demo/__init__.py
+++ src/hatch_demo/__init__.py
@@ -1,3 +1,19 @@
 # SPDX-FileCopyrightText: 2024-present MtkN1 <51289448+MtkN1@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
+import sys
+
+
+def print_version():
+    version_info = tuple(sys.version_info[:2])
+
+    if version_info == (3, 8):
+        print("You are running Python 3.8")
+    elif version_info == (3, 9):
+        print("You are running Python 3.9")
+    elif version_info == (3, 10):
+        print("You are running Python 3.10")
+    elif version_info == (3, 11):
+        print("You are running Python 3.11")
+    else:
+        print("You are running Python >= 3.12")
---
+++ tests/test_cov.py
@@ -0,0 +1,5 @@
+import hatch_demo
+
+
+def test_cov():
+    hatch_demo.print_version()
