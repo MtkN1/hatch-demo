05093753efd8db9f5b9c94bcd4b10c399b4d418e
machuken <51289448+MtkN1@users.noreply.github.com>
2024-05-10 14:03:49 +0900

    Create ci.yml

--- src/hatch_demo/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present MtkN1 <51289448+MtkN1@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present MtkN1 <51289448+MtkN1@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
